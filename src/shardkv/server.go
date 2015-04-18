bfc4b46790e2377e4341d3c1a6fba7fbb4a1eb3d
Ankush Gupta <me@ankushg.com>
2015-04-18 13:23:34 -0400

    workingish shardkv server

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -16,24 +16,29 @@ import "shardmaster"
 
 const Debug = 0
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+func (kv *ShardKV) DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
-		log.Printf(format, a...)
+		kv.log_mu.Lock()
+		defer kv.log_mu.Unlock()
+
+		fmt.Printf("\x1b[%dm", (kv.me%6)+31)
+		fmt.Printf("(Group: %d, Server: %d, Config: %d):\t", kv.gid, kv.me, kv.config.Num)
+		fmt.Printf(format+"\n", a...)
+		fmt.Printf("\x1b[0m")
 	}
 	return
 }
 
 type Op struct {
-	// Your definitions here.
 	Type      OpType
 	Id        int64
 	Key       string
 	Value     string
-	ConfigNum int
+	CurConfig shardmaster.Config
 
 	// for reconfiguration
 	KvData   map[string]string
-	PreReply map[int64]string
+	LastSeen map[int64]int64
 }
 
 type ShardKV struct {
@@ -45,31 +50,238 @@ type ShardKV struct {
 	sm         *shardmaster.Clerk
 	px         *paxos.Paxos
 
-	gid int64 // my replica group ID
+	log_mu sync.Mutex // just used for colorful logs
+
+	gid int64 // my replica group Id
 
 	config          shardmaster.Config
 	kvData          map[string]string
-	preReply        map[int64]string
+	lastSeen        map[int64]int64
+	maxInstance     int
 	maxProcessedSeq int
+
+	// Reconfiguration
+	highestSeen      int
+	currentConfigNum int
+	reconfigKvData   map[string]string
 }
 
+// -----------------------------------------------------
+// Handle RPCs
+// -----------------------------------------------------
+
 func (kv *ShardKV) Get(args *GetArgs, reply *Reply) error {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.DPrintf("Get for %s received (%s)", args.Key, reply.Err)
+
+	if kv.config.Shards[key2shard(args.Key)] == kv.gid {
+		if kv.lastSeen[args.ClientId] != args.TransactionId { // at most once
+			op := Op{Key: args.Key, Type: GetOp, Id: nrand()}
+			kv.logOp(op)
+		}
+
+		if val, ok := kv.kvData[args.Key]; ok {
+			reply.Value = val
+			reply.Err = OK
+		} else {
+			reply.Err = ErrNoKey
+		}
+	} else {
+		reply.Err = ErrWrongGroup
+	}
 	return nil
 }
 
 // RPC handler for client Put and Append requests
-func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) error {
-	// Your code here.
+func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *Reply) error {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.DPrintf("%s for %s received (%s)", args.Type, args.Key, reply.Err)
+
+	if kv.lastSeen[args.ClientId] != args.TransactionId {
+		op := Op{Key: args.Key, Value: args.Value, Type: args.Type, Id: nrand()}
+		kv.logOp(op)
+
+		if kv.config.Shards[key2shard(args.Key)] == kv.gid {
+			reply.Err = OK
+		} else {
+			reply.Err = ErrWrongGroup
+		}
+	}
 	return nil
 }
+
+func (kv *ShardKV) FetchShard(args *FetchShardArgs, reply *FetchShardReply) error {
+	kv.DPrintf("Fetch for config %s, shard %s received (%s)", args.Config, args.Shard, reply.Err)
+
+	if args.Config < kv.config.Num {
+		kv.mu.Lock()
+		defer kv.mu.Unlock()
+
+		shardKvData := make(map[string]string)
+		for k, v := range kv.kvData {
+			if key2shard(k) == args.Shard {
+				shardKvData[k] = v
+			}
+		}
+
+		reply.KvData = shardKvData
+		reply.LastSeen = kv.lastSeen
+		reply.Err = OK
+
+		op := Op{Type: FetchShardOp, Id: nrand()}
+		kv.logOp(op)
+	} else {
+		reply.Err = ErrBadConfig
+	}
+	return nil
+}
+
+// Actually apply an operation to local state
+func (kv *ShardKV) doOp(op Op) {
+	kv.DPrintf("Applying log entry %s of type %s", op.Id, op.Type)
+	switch op.Type {
+	case GetOp:
+		// we don't need to do anything here
+	case PutOp:
+		if kv.config.Shards[key2shard(op.Key)] == kv.gid {
+			kv.kvData[op.Key] = op.Value
+		}
+	case AppendOp:
+		if kv.config.Shards[key2shard(op.Key)] == kv.gid {
+			kv.kvData[op.Key] += op.Value
+		}
+	case ReconfigOp:
+		if kv.config.Num < op.CurConfig.Num {
+			for k, v := range op.KvData {
+				kv.kvData[k] = v
+			}
+			for k, v := range op.LastSeen {
+				kv.lastSeen[k] = v
+			}
+			kv.config = op.CurConfig
+		}
+	}
+}
+
+// ------------------------------------------------
+// Checking for new configurations
+// ------------------------------------------------
 
 //
 // Ask the shardmaster if there's a new configuration;
 // if so, re-configure.
 //
 func (kv *ShardKV) tick() {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	config := kv.sm.Query(-1)
+
+	if config.Num > kv.config.Num {
+		for i := kv.config.Num + 1; i <= config.Num; i++ {
+			err := kv.reconfigure(kv.sm.Query(i))
+			if err != OK {
+				return
+			}
+		}
+	}
 }
+
+func (kv *ShardKV) reconfigure(config shardmaster.Config) Err {
+	kv.DPrintf("Reconfiguring...")
+
+	shardsKvData := make(map[string]string)
+	shardsLastSeen := make(map[int64]int64)
+
+	for i := 0; i < shardmaster.NShards; i++ {
+		// get new data from previous owner
+		prevOwner := kv.config.Shards[i]
+		if config.Shards[i] == kv.gid && prevOwner != kv.gid && prevOwner != 0 {
+			args := &FetchShardArgs{Shard: i, Config: kv.config.Num}
+			var reply FetchShardReply
+			gid := kv.config.Shards[i]
+			servers, ok := kv.config.Groups[gid]
+			if ok {
+				for index := range servers {
+					ok := call(servers[index], "ShardKV.FetchShard", args, &reply)
+					if ok && reply.Err == OK {
+						for k, v := range reply.KvData {
+							shardsKvData[k] = v
+						}
+						for k, v := range reply.LastSeen {
+							shardsLastSeen[k] = v
+						}
+						break
+					}
+				}
+				if reply.Err == ErrBadConfig {
+					return ErrBadReconfig
+				}
+			} else {
+				return ErrBadReconfig
+			}
+		}
+	}
+	reconfigOp := Op{Type: ReconfigOp, Id: nrand(), CurConfig: config, KvData: shardsKvData, LastSeen: shardsLastSeen}
+
+	kv.logOp(reconfigOp)
+	return OK
+}
+
+// ------------------------------------------------
+// Communicate with Paxos log
+// ------------------------------------------------
+func (kv *ShardKV) logOp(transaction Op) {
+	kv.DPrintf("Logging op %s (%s)...", transaction.Id, transaction.Type)
+
+	var seq int
+	for !kv.isdead() {
+		seq = kv.maxInstance + 1
+
+		fate, value := kv.px.Status(seq)
+		var val Op
+
+		if fate != paxos.Decided {
+			kv.px.Start(seq, transaction)
+			val = kv.poll(seq)
+		} else {
+			val = value.(Op)
+		}
+
+		kv.doOp(val)
+		kv.px.Done(seq)
+		kv.maxInstance++
+
+		if val.Id == transaction.Id {
+			return
+		}
+	}
+}
+
+func (kv *ShardKV) poll(seq int) Op {
+	to := InitialBackoff
+	for !kv.isdead() {
+		fate, value := kv.px.Status(seq)
+
+		if fate == paxos.Decided {
+			return value.(Op)
+		}
+
+		time.Sleep(to)
+		if to < MaxBackoff {
+			to *= 2
+		}
+	}
+	return Op{}
+}
+
+// ---------------------------------------------
+// Boilerplate staff code
+// --------------------------------------------
 
 // tell the server to shut itself down.
 // please don't change these two functions.
@@ -114,7 +326,16 @@ func StartServer(gid int64, shardmasters []string,
 	kv.gid = gid
 	kv.sm = shardmaster.MakeClerk(shardmasters)
 
-	// Your initialization code here.
+	kv.config = shardmaster.Config{Num: -1}
+	kv.kvData = make(map[string]string)
+	kv.lastSeen = make(map[int64]int64)
+	kv.maxInstance = -1
+	kv.maxProcessedSeq = -1
+
+	// Reconfiguration
+	kv.highestSeen = -1
+	kv.reconfigKvData = make(map[string]string)
+
 	// Don't call Join().
 
 	rpcs := rpc.NewServer()
